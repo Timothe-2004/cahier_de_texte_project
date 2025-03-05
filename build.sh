eaff4ac838eb271ddd96bc58be711be606fd455b
Timothe-2004 <timothejames2004@gmail.com>
2025-03-05 10:44:11 +0100

    Update Vercel configuration and build process

--- build.sh
+++ build.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+
+# Mise à jour de pip
+python -m pip install --upgrade pip
+
+# Installation des dépendances
 pip install -r requirements.txt
+
+# Vérification de l'installation de crispy-forms
+pip show django-crispy-forms
+pip show crispy-bootstrap5
+
+# Collecte des fichiers statiques
+python manage.py collectstatic --noinput
+
 python manage.py migrate
